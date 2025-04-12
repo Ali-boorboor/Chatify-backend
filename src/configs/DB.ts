855526cfe13a667425689f9d0a3fd095401a4174
ADROEX .TSX <ali.w01rk@gmail.com>
2025-04-12 17:34:53 +0330

    deploy configs added

--- src/configs/DB.ts
+++ src/configs/DB.ts
@@ -4,7 +4,9 @@ import type { FastifyInstance } from "fastify";
 // ^ connect to db function
 const dbConnection = async (fastify: FastifyInstance) => {
   try {
-    await mongoose.connect(process.env.CONNECTION_STRING!);
+    await mongoose.connect(process.env.CONNECTION_STRING!, {
+      authSource: "admin",
+    });
     fastify.log.info("Connect to db successfully");
   } catch (err: any) {
     fastify.log.error(err?.message);
